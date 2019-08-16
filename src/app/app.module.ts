9b51ea023c9c6ebe1ec6d767bedb37f0e105c1a1
phxluke <lkrumtinger@gmail.com>
2019-08-16 16:36:30 -0700

    6 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core'
 import { routes } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
@@ -10,7 +12,7 @@ import { AboutComponent } from './about/about.component';
 import { BlogComponent } from './blog/blog.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, MatCardModule, routes],
+  imports:      [ BrowserModule, FormsModule, MatCardModule, FontAwesomeModule, routes],
   declarations: [ AppComponent, HelloComponent, HomeComponent, AboutComponent, BlogComponent ],
   bootstrap:    [ AppComponent ]
 })
